97a28256ec7298ed25ccffa739fc35d5f78bc55b
stephen Mwangi <stephemmw@gmail.com>
2025-03-03 07:17:19 +0300

    Add bootstrap, tailwind and font files.

---
+++ app/static/tailwindcss/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config}  v3.14.7*/
+module.exports = {
+    prefix: "tw-",
+    content: [
+        "../../templates/**/*.html",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
+
