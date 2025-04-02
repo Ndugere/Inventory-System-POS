8e43b66c717d8880674ad6d7abbafb6ecfb0491b
Stephen Mwangi <stephemmw@gmail.com>
2025-04-02 14:38:52 +0300

    Home UI update

--- posApp/urls.py
+++ posApp/urls.py
@@ -4,9 +4,10 @@ from django.contrib.auth import views as auth_views
 from django.urls import path
 from django.views.generic.base import RedirectView
 
+#app_name = "posApp"
 urlpatterns = [
     path('redirect-admin', RedirectView.as_view(url="/admin"),name="redirect-admin"),
-    path('', views.home, name="home-page"),
+    path('', report_views2.home, name="home-page"),
     path('login', auth_views.LoginView.as_view(template_name = 'posApp/login.html',redirect_authenticated_user=True), name="login"),
     path('userlogin', views.login_user, name="login-user"),
     path('logout', views.logoutuser, name="logout"),
